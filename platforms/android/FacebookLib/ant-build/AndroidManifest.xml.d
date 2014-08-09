f83b132b4730799bd27127b34ec3936985e42012
Guy Sopher <guysopher@gmail.com>
2014-08-09 05:20:19 +0300

    ionic build

---
+++ platforms/android/FacebookLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/guy/Projects/TreatsForLife/mobile/platforms/android/FacebookLib/ant-build/AndroidManifest.xml : \
+/home/guy/Projects/TreatsForLife/mobile/platforms/android/FacebookLib/AndroidManifest.xml \
--- platforms/android/ant-build/TreatsForLife-debug-unaligned.apk.d
+++ platforms/android/ant-build/TreatsForLife-debug-unaligned.apk.d
@@ -2,3 +2,6 @@
 /home/guy/Projects/TreatsForLife/mobile/platforms/android/ant-build/TreatsForLife.ap_ \
 /home/guy/Projects/TreatsForLife/mobile/platforms/android/ant-build/classes.dex \
 /home/guy/Projects/TreatsForLife/mobile/platforms/android/CordovaLib/ant-build/classes.jar \
+/home/guy/Projects/TreatsForLife/mobile/platforms/android/FacebookLib/ant-build/classes.jar \
+/home/guy/Projects/TreatsForLife/mobile/platforms/android/FacebookLib/libs/bolts.jar \
+/home/guy/Projects/TreatsForLife/mobile/platforms/android/FacebookLib/libs/android-support-v4.jar \

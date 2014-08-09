d556f2e426b855594032c7eb7223f6e096875ebb
Guy Sopher <guysopher@gmail.com>
2014-08-09 22:33:05 +0300

    ionic build

--- www/js/app.js
+++ www/js/app.js
@@ -26,7 +26,7 @@ angular.module('clientApp', ['ionic',
         });
     })
 
-    .config(function ($stateProvider, $urlRouterProvider, $location) {
+    .config(function ($stateProvider, $urlRouterProvider) {
 
         // Ionic uses AngularUI Router which uses the concept of states
         // Learn more here: https://github.com/angular-ui/ui-router
@@ -98,13 +98,13 @@ angular.module('clientApp', ['ionic',
             console.log('localStorage', localStorage);
             if (!localStorage.fb_id || $location.search()['s'] == 'w') {
                 console.log('ROUTER: Redirecting to Welcome');
-                $location.path('/welcome');
+                return ('/welcome');
             } else if (!localStorage.user_pet_id) {
                 console.log('ROUTER: Redirecting to Pets');
-                $location.path('/pets');
+                return ('/pets');
             } else {
                 console.log('ROUTER: Redirecting to Pet');
-                $location.path('/pet');
+                return ('/pet');
             }
         });
 

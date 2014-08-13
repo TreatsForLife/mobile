528dc93d2b0dd15bb67438bb88eef67f30752cf4
Guy Sopher <guysopher@gmail.com>
2014-08-13 23:20:51 +0300

    plugin bug fixed?

--- platforms/android/FacebookLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/guy/Projects/TreatsForLife/mobile/platforms/android/FacebookLib/bin/AndroidManifest.xml : \
-/home/guy/Projects/TreatsForLife/mobile/platforms/android/FacebookLib/AndroidManifest.xml \

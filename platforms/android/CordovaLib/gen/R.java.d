7a4c193e5f0907ff23c10212ba4c5aa89f4963da
Guy Sopher <guysopher@gmail.com>
2014-08-21 00:29:27 +0300

    ionic build

--- platforms/android/CordovaLib/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /home/guy/Projects/TreatsForLife/mobile/platforms/android/CordovaLib/bin/AndroidManifest.xml \
--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/guy/Projects/TreatsForLife/mobile/platforms/android/bin/AndroidManifest.xml : \
-/home/guy/Projects/TreatsForLife/mobile/platforms/android/AndroidManifest.xml \

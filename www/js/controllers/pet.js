825e2983674b5e6ae53b6b7e37f5de051471275b
guy@moment.me <guysopher@gmail.com>
2014-11-15 21:21:44 +0200

    minor bug

--- www/js/controllers/pet.js
+++ www/js/controllers/pet.js
@@ -189,7 +189,7 @@ angular.module('clientApp')
             if (!$scope.showButton) return;
             $rootScope.runAnimation('.pet-buy-button', 1700, 48, $scope.buttonHeight, function () {
                 if (window.ionic.Platform.isIOS()){
-                    $scope.goto(Consts.client_root + 'shop/' + $scope.pet_id);
+                    $scope.goto(Consts.client_root + '#/shop/' + $scope.pet_id);
                 }else{
                     $location.path('/shop/' + $scope.pet_id);
                 }
@@ -232,7 +232,7 @@ angular.module('clientApp')
             $('.pet-adopt-button').show();
             $rootScope.runAnimation('.pet-adopt-button', 1700, 34, $scope.buttonHeight, function () {
                 if (window.ionic.Platform.isIOS()){
-                    $scope.goto(Consts.client_root + 'shop/' + $scope.pet_id);
+                    $scope.goto(Consts.client_root + '#/shop/' + $scope.pet_id);
                 }else{
                     $location.path('/shop/' + $scope.pet_id);
                 }

8f7e2b74fc9bc19f6a8af4d3f18a1082f4ce225b
gilsefti <gilsefti@gmail.com>
2015-01-18 16:11:26 +0200

    Revert "Fix routing for local login"
    This reverts commit 2ecad79ff3e5e0974aaeb60f40936d797e0f9a01.

--- www/js/services/utils.js
+++ www/js/services/utils.js
@@ -1,11 +1,11 @@
 var Utils = {
-    isLocal: true,//(document.location.host.search('localhost') > -1) || (document.location.host.search('lvh.me') > -1),
+    isLocal: (document.location.host.search('localhost') > -1) || (document.location.host.search('lvh.me') > -1),
     isBchmn: (document.location.host.search('bchmn.com') > -1),
     isHeroku: (document.location.host.search('herokuapp.com') > -1),
-    findIdInArray: function (arr, idVal, idKey) {
+    findIdInArray: function(arr, idVal, idKey){
         if (typeof idKey == 'undefined') idKey = '_id';
-        for (var i = 0, a; a = arr[i]; i++) {
-            if (a[idKey] == idVal) {
+        for (var i=0, a; a = arr[i]; i++){
+            if (a[idKey] == idVal){
                 return a;
             }
         }
@@ -13,12 +13,10 @@ var Utils = {
     }
 }
 
-
 var Consts = {
     debug: true,
-    api_root: navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/) ? 'http://10.0.2.2:3000/' : 'http://localhost:3000/',
-    // api_root: Utils.isLocal ?   'http://localhost:3000/': 'http://treatsforlife-api.herokuapp.com/',//'http://tfl.bchmn.com/',//'http://treatsforlife-api.herokuapp.com/','http://10.0.2.2:3000/','http://localhost:3000/'
-    client_root: Utils.isLocal ? 'http://localhost:9000/' : 'http://app.treatsforlife.org/',
+    api_root: Utils.isLocal ? 'http://localhost:3000/' : 'http://treatsforlife-api.herokuapp.com/',//'http://tfl.bchmn.com/',//'http://treatsforlife-api.herokuapp.com/'
+    client_root: Utils.isLocal ? 'http://localhost:9000/': 'http://app.treatsforlife.org/',
     fb_app_id: Utils.isLocal ? '262700727225341' : '601219569953172'
 }
 
